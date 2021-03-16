beb1d450dcb14be5282b832b96624d87dc20e3f3
KarandeepSinghCoder <karandchandi@gmail.com>
2021-03-16 19:59:28 +0530

    dashboard added

--- src/dashboard/dashboardControler.js
+++ src/dashboard/dashboardControler.js
@@ -6,4 +6,11 @@ router.get("/", function(req, res) {
   res.render("dashboard/dashboard.ejs");
 });
 
+router.get("/profile", function(req, res) {
+  res.render("dashboard/profile.ejs");
+});
+
+router.get("/speeches", function(req, res) {
+  res.render("dashboard/speeches.ejs");
+});
 module.exports = router;
