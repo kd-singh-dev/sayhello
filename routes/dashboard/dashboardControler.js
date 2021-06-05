e23868dbf4e51ae79b684e8f6a8e2b8d8ed48144
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2021-06-05 21:13:20 +0530

    site css improved

--- routes/dashboard/dashboardControler.js
+++ routes/dashboard/dashboardControler.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
-const { ensureAuthenticated, forwardAuthenticated } = require('../../config/auth');
+const {
+  ensureAuthenticated,
+  forwardAuthenticated,
+} = require("../../config/auth");
 var Speech = require("../../models/Speech");
 var User = require("../../models/User");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-
 
 //MULTER
 
@@ -27,106 +29,105 @@ const upload = multer({
   storage: storage,
 }).single("ProfilePic");
 // Dashboard
-router.get('/', ensureAuthenticated, (req, res) =>
-  res.render('dashboard/dashboard', {
-    user: req.user
+router.get("/", ensureAuthenticated, (req, res) =>
+  res.render("dashboard/dashboard", {
+    user: req.user,
   })
 );
-router.get('/speeches', ensureAuthenticated, (req, res) =>
-  Speech.find({UserID:req.user.id},function(err, allSpeech){
-    if(err){
+router.get("/speeches", ensureAuthenticated, (req, res) =>
+  Speech.find({ UserID: req.user.id }, function (err, allSpeech) {
+    if (err) {
       console.log(err);
-    }else{
-      res.render('dashboard/speeches', {user: req.user,allSpeech:allSpeech});
-      // console.log(allSpeech);
+    } else {
+      res.render("dashboard/speeches", {
+        user: req.user,
+        allSpeech: allSpeech,
+      });
     }
-  }));
+  })
+);
 
-router.post('/speech',ensureAuthenticated,(req, res)=>{
-  
+router.post("/speech", ensureAuthenticated, (req, res) => {
   var title = req.body.title;
-  var speech=req.body.speech;
-  var newSpeech={UserID: req.user._id,Speechname: title,Speech:speech};
-  Speech.create(newSpeech,function(err,newlyCreated){
-    if(err){
+  var speech = req.body.speech;
+  var newSpeech = { UserID: req.user._id, Speechname: title, Speech: speech };
+  Speech.create(newSpeech, function (err, newlyCreated) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.redirect("/dashboard");
     }
-  })
-  
+  });
 });
-router.post('/delspeech',ensureAuthenticated,(req, res)=>{
+router.post("/delspeech", ensureAuthenticated, (req, res) => {
   var delSpeech = req.body.delSpeech;
-  Speech.findOneAndDelete({_id:delSpeech},function(err,docs){
-    if (err){
-      console.log(err)
-  }
-  else{
-      console.log("Deleted User : ", docs);
-      res.redirect("/dashboard/speeches")
-  }
-  })
-  
-});
-router.get('/editspeech', ensureAuthenticated, (req, res) =>{
-  var _id=req.query.editspeech;
-  Speech.findById(_id, function(err, result) {
+  Speech.findOneAndDelete({ _id: delSpeech }, function (err, docs) {
     if (err) {
-      res.send(err);
-    } else {
-      res.render('dashboard/editspeech', {user: req.user, speech:result})
-    }
-  });
-}
-);
-router.get('/signlanguage', ensureAuthenticated, (req, res) =>{
-  var _id=req.query.signlanguage;
-  Speech.findById(_id, function(err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.render('dashboard/signlanguage', {user: req.user, speech:result})
-    }
-  });
-}
-);
-router.post('/updatespeech', ensureAuthenticated, (req, res) =>{
-
-  id= req.body.id;
-  speechname=req.body.Speechname;
-  speech=req.body.speech;
-  console.log(id+"\n"+speechname+"\n"+speech);
-  Speech.findByIdAndUpdate(id, {Speechname:speechname, Speech:speech },function (err, docs) {
-    if (err){
-        console.log(err)
-    }
-    else{
-        console.log("Updated User : ", docs);
-        res.redirect("/dashboard/speeches");
-    }
-});
-}
-);
-router.get('/profile', ensureAuthenticated, (req, res) =>{
-
-  Speech.count({ UserID: req.user._id }, function (err, Totalcon) {
-    if(err){
       console.log(err);
-    }else{
-
-      User.findOneAndUpdate({ _id: req.user._id },{ $inc: { PageView: 1 } },{ new: true },function (err, response) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render('dashboard/profile', {user: req.user,Totalcon: Totalcon});
-        }
-      });
-
+    } else {
+      res.redirect("/dashboard/speeches");
     }
   });
-}
-);
+});
+router.get("/editspeech", ensureAuthenticated, (req, res) => {
+  var _id = req.query.editspeech;
+  Speech.findById(_id, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.render("dashboard/editspeech", { user: req.user, speech: result });
+    }
+  });
+});
+router.get("/signlanguage", ensureAuthenticated, (req, res) => {
+  var _id = req.query.signlanguage;
+  Speech.findById(_id, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.render("dashboard/signlanguage", { user: req.user, speech: result });
+    }
+  });
+});
+router.post("/updatespeech", ensureAuthenticated, (req, res) => {
+  id = req.body.id;
+  speechname = req.body.Speechname;
+  speech = req.body.speech;
+  Speech.findByIdAndUpdate(
+    id,
+    { Speechname: speechname, Speech: speech },
+    function (err, docs) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/dashboard/speeches");
+      }
+    }
+  );
+});
+router.get("/profile", ensureAuthenticated, (req, res) => {
+  Speech.count({ UserID: req.user._id }, function (err, Totalcon) {
+    if (err) {
+      console.log(err);
+    } else {
+      User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $inc: { PageView: 1 } },
+        { new: true },
+        function (err, response) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("dashboard/profile", {
+              user: req.user,
+              Totalcon: Totalcon,
+            });
+          }
+        }
+      );
+    }
+  });
+});
 
 router.post("/updateprofilepic", ensureAuthenticated, (req, res) => {
   upload(req, res, (err) => {
@@ -137,7 +138,6 @@ router.post("/updateprofilepic", ensureAuthenticated, (req, res) => {
       var path = "/images/" + req.file.filename;
       // Delete the Previous file
       var previouspath = "./public" + req.user.ProfilePic;
-      //  console.log("previous path is : "+previouspath);
       fs.unlink(previouspath, (err) => {
         if (err) {
           console.error(err);
@@ -147,7 +147,7 @@ router.post("/updateprofilepic", ensureAuthenticated, (req, res) => {
       });
       User.findByIdAndUpdate(
         req.user.id,
-        { ProfilePic: path},
+        { ProfilePic: path },
         function (err, docs) {
           if (err) {
             console.log(err);
@@ -162,16 +162,13 @@ router.post("/updateprofilepic", ensureAuthenticated, (req, res) => {
 
 router.post("/updateprofile", ensureAuthenticated, (req, res) => {
   var name = req.body.username;
-  User.findByIdAndUpdate(req.user._id,{name: name},function(err, docs) {
-    if(err){
+  User.findByIdAndUpdate(req.user._id, { name: name }, function (err, docs) {
+    if (err) {
       console.log(err);
-    }else{
-      console.log(docs);
+    } else {
       res.redirect("/dashboard/profile");
     }
-  })
+  });
 });
-
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,53 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 // Load User model
-const User = require('../models/User');
-const { forwardAuthenticated } = require('../config/auth');
+const User = require("../models/User");
+const { forwardAuthenticated } = require("../config/auth");
 
 // Login Page
-router.get('/login', forwardAuthenticated, (req, res) => res.render('auth/login'));
+router.get("/login", forwardAuthenticated, (req, res) =>
+  res.render("auth/login")
+);
 
 // Register Page
-router.get('/register', forwardAuthenticated, (req, res) => res.render('auth/register'));
+router.get("/register", forwardAuthenticated, (req, res) =>
+  res.render("auth/register")
+);
 
 // Register
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
   if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
+    errors.push({ msg: "Please enter all fields" });
   }
 
   if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
+    errors.push({ msg: "Passwords do not match" });
   }
 
   if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
+    errors.push({ msg: "Password must be at least 6 characters" });
   }
 
   if (errors.length > 0) {
-    res.render('auth/register', {
+    res.render("auth/register", {
       errors,
       name,
       email,
       password,
-      password2
+      password2,
     });
   } else {
-    User.findOne({ email: email }).then(user => {
+    User.findOne({ email: email }).then((user) => {
       if (user) {
-        errors.push({ msg: 'Email already exists' });
-        res.render('auth/register', {
+        errors.push({ msg: "Email already exists" });
+        res.render("auth/register", {
           errors,
           name,
           email,
           password,
-          password2
+          password2,
         });
       } else {
         const newUser = new User({
@@ -55,7 +59,7 @@ router.post('/register', (req, res) => {
           email,
           password,
           ProfilePic: "https://i.imgur.com/0cURfPH.png",
-          PageView: 1
+          PageView: 1,
         });
 
         bcrypt.genSalt(10, (err, salt) => {
@@ -64,14 +68,14 @@ router.post('/register', (req, res) => {
             newUser.password = hash;
             newUser
               .save()
-              .then(user => {
+              .then((user) => {
                 req.flash(
-                  'success_msg',
-                  'You are now registered and can log in'
+                  "success_msg",
+                  "You are now registered and can log in"
                 );
-                res.redirect('/users/login');
+                res.redirect("/users/login");
               })
-              .catch(err => console.log(err));
+              .catch((err) => console.log(err));
           });
         });
       }
@@ -80,19 +84,19 @@ router.post('/register', (req, res) => {
 });
 
 // Login
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/users/login',
-    failureFlash: true
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
   })(req, res, next);
 });
 
 // Logout
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
-  req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
